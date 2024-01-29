428235a0eb7154cfd8b842ce352ceefaa88e358e
Agorablood <eli0002@alu.medac.es>
2024-01-29 11:47:56 +0100

    añadido el favicon

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba BBDD</title>
-    <link rel="shortcut icon" href="public/logo/aguilawallapop.jpg" type="image/x-icon">
+    <title>PopWalla</title>
+    <link rel="icon" href="<?php echo base_url() ?>aguilawallapop.ico" size="16x16" type="image/png">
+    <link rel="icon" href="<?php echo base_url() ?>aguilawallapop.ico" size="32x32" type="image/png">
     <link rel="stylesheet" href="<?php echo base_url("css/estilos.css") ?>" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="<?php echo base_url('js/jquery.js') ?>">
